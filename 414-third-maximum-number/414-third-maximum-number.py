4a3ac9336b77d0bae1898033da29631f907cc865
HyoungSeok Kim <khs96p@gmail.com>
2022-08-22 16:38:18 +0900

    Time: 4734 ms (5.02%), Space: 223.2 MB (21.29%) - LeetHub

---
+++ 414-third-maximum-number/414-third-maximum-number.py
@@ -0,0 +1,27 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        """
+        param:
+            nums: an integer array
+        return: the third distinct maximum number in the nums, 
+                if the third maximum does not exist, return the maximum number
+        """
+        unique_nums=set(nums)
+        
+        sorted_nums=self.quick_sort(list(unique_nums))
+        
+        if len(sorted_nums)>=3:
+            return sorted_nums[-3]
+        else:
+            return sorted_nums[-1]
+        
+    def quick_sort(self, lst):
+        if len(lst)<=1:
+            return lst
+        
+        pivot, tail = lst[0], lst[1:]
+        
+        left=[x for x in tail if x<=pivot]
+        right=[x for x in tail if x>pivot]
+        
+        return self.quick_sort(left) + [pivot] + self.quick_sort(right)
