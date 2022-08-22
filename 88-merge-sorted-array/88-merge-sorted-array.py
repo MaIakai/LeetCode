1e113d514bde3790f1a1cfcec4966e6ac72bde79
HyoungSeok Kim <khs96p@gmail.com>
2022-08-22 17:04:18 +0900

    Time: 77 ms (13.34%), Space: 14 MB (5.82%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -0,0 +1,45 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        
+        param:
+            nums1, nums2: non-decreasing order
+            m: number of elements in nums1
+            n: number of elements in nums2
+        
+        return: Modify nums1 in-place
+        """
+        if len(nums1)==1 and nums1[-1] == 0:
+            nums3=nums2
+        else:
+            nums3=nums1[:len(nums1)-n]+nums2
+        nums3=self.merge_sort(nums3)
+        
+        del nums1[:]
+        nums1.extend(nums3)
+        
+    def merge_sort(self, lst):
+        if len(lst) < 2:
+            return lst
+        
+        mid=len(lst) // 2
+        left=self.merge_sort(lst[:mid])
+        right=self.merge_sort(lst[mid:])
+        
+        merged_lst=[]
+        
+        l=0
+        r=0
+        
+        while l<len(left) and r < len(right):
+            if left[l] < right[r]:
+                merged_lst.append(left[l])
+                l+=1
+            else:
+                merged_lst.append(right[r])
+                r+=1
+        merged_lst += left[l:]
+        merged_lst += right[r:]
+        
+        return merged_lst
