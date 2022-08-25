9268d0f0ee2b395ca9f4a532e135cdb294891c5a
HyoungSeok Kim <khs96p@gmail.com>
2022-08-25 23:04:45 +0900

    Time: 6144 ms (35.54%), Space: 67.6 MB (44.92%) - LeetHub

---
+++ 204-count-primes/204-count-primes.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        """
+        param:
+            n: an integer
+        
+        return the number of prime numbers that are less than n
+        """
+        if n<=2:
+            return 0
+        primes=[True for i in range(n+1)]
+        i=2
+        while i*i<n:
+            if primes[i]:
+                for j in range(i*2, n, i):
+                    primes[j]=False
+                    j+=1
+            i+=1
+            
+        ans=0
+        for i in range(2, n):
+            if primes[i]==True:
+                ans+=1
+            
+        
+                
+        return ans
+            
+        
